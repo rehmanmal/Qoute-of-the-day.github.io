4474a1d6449dcf2ae36949f012d655179b74f771
Rehman Ali <ma8240212@gmail.com>
2023-10-09 09:46:52 +0500

    genrate daily free quote

---
+++ script.js
@@ -0,0 +1,13 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random"
+async function getqoute(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+getqoute(api_url);
+function tweet(){
+    window.open("https://twitter.com/intent/tweet/?text=" + quote.innerHTML + "---by " + author.innerHTML, "Tweet Window", "width=600, height=300");
+}
